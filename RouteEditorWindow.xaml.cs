62537c43e1beef571d036a1a71e3403ca501c64c
AdautoEC <adauto.ec@gmail.com>
2025-10-20 09:13:48 -0300

    feat: adicionando funcionalidade de adicionar e remover endereco de rota

--- RouteEditorWindow.xaml.cs
+++ RouteEditorWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,13 +11,13 @@ namespace CsvIntegratorApp
     public partial class RouteEditorWindow : Window
     {
         private readonly ModelRow _modelRow;
-        private readonly List<WaypointInfo> _waypoints;
+        private readonly ObservableCollection<WaypointInfo> _waypoints;
 
         public RouteEditorWindow(ModelRow modelRow)
         {
             InitializeComponent();
             _modelRow = modelRow;
-            _waypoints = modelRow.Waypoints;
+            _waypoints = new ObservableCollection<WaypointInfo>(modelRow.Waypoints);
             WaypointsGrid.ItemsSource = _waypoints;
         }
 
@@ -25,7 +26,7 @@ namespace CsvIntegratorApp
             SaveButton.IsEnabled = false;
             ProgressBar.Visibility = Visibility.Visible;
 
-            var updatedWaypoints = (List<WaypointInfo>)WaypointsGrid.ItemsSource;
+            var updatedWaypoints = _waypoints.ToList();
 
             var routeResult = await DistanceService.TryRouteLegsKmAsync(updatedWaypoints, true);
 
@@ -34,6 +35,7 @@ namespace CsvIntegratorApp
                 _modelRow.DistanciaPercorridaKm = routeResult.TotalKm;
                 _modelRow.Roteiro = string.Join(" -> ", updatedWaypoints.Select(w => w.City));
                 _modelRow.MapPath = RouteLogService.GenerateRouteMap(routeResult.Polyline, routeResult.Waypoints, new List<ModelRow> { _modelRow });
+                _modelRow.Waypoints = updatedWaypoints; // Atualiza a lista original
             }
             else
             {
@@ -45,6 +47,23 @@ namespace CsvIntegratorApp
 
             DialogResult = true;
             Close();
+        }
+
+        private void AddAddressButton_Click(object sender, RoutedEventArgs e)
+        {
+            _waypoints.Add(new WaypointInfo { Address = "Novo Endereço" });
+        }
+
+        private void RemoveAddressButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WaypointsGrid.SelectedItem is WaypointInfo selectedWaypoint)
+            {
+                _waypoints.Remove(selectedWaypoint);
+            }
+            else
+            {
+                MessageBox.Show(this, "Selecione um endereço para remover.", "Nenhum Endereço Selecionado", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
